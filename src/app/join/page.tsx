a538734a2bb39cbc0dd4f8fdaefeb52d40fe65a6
greeneeya <suzythetravelerr@gmail.com>
2024-03-22 12:11:00 +0900

    [LINGDA-9] feat: Headless Input Component with React Hook Form

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -1,27 +1,47 @@
+'use client';
 import React from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import InputGroup from './InputGroup';
 
 export default function Join() {
+  type LoginInputs = {
+    id: string;
+    pw: string;
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginInputs>();
+  const onSumbit: SubmitHandler<LoginInputs> = (data) => console.log(data);
+
   return (
     <div className="flex flex-col gap-2">
-      <form className="flex flex-col gap-2">
-        <div className="flex flex-col gap-1">
-          <label htmlFor="id">아이디</label>
-          <input
-            placeholder="아이디"
-            name="id"
-            type="text"
-            className="border rounded-sm p-2"
-          />
-        </div>
-        <div className="flex flex-col gap-1">
-          <label htmlFor="pw">비밀번호</label>
-          <input
-            placeholder="비밀번호"
-            name="pw"
-            type="password"
-            className="border rounded-sm p-2"
-          />
-        </div>
+      <form className="flex flex-col gap-2" onSubmit={handleSubmit(onSumbit)}>
+        <InputGroup
+          type="text"
+          name="id"
+          placeholder="아이디"
+          label="아이디"
+          error={errors.id}
+          register={() =>
+            register('id', {
+              required: { value: true, message: '아이디를 입력해주세요' },
+            })
+          }
+        />
+        <InputGroup
+          type="password"
+          name="pw"
+          placeholder="비밀번호"
+          label="비밀번호"
+          error={errors.pw}
+          register={() =>
+            register('pw', {
+              required: { value: true, message: '비밀번호를 입력해주세요' },
+            })
+          }
+        />
         <button type="submit" className="bg-blue-300 p-2 rounded-lg">
           회원가입하기
         </button>
