1bb720fdf9f61c49f0f839427a9901d0b4e2f9b5
greeneeya <suzythetravelerr@gmail.com>
2024-05-22 00:28:27 +0900

    feat: 사진 클릭시 Modal 띄움

--- src/app/topic/[id]/page.tsx
+++ src/app/topic/[id]/page.tsx
@@ -12,6 +12,8 @@ type Props = {
 export default function Page({ params: { id } }: Props) {
   const { topicDetailQuery } = useTopic();
   const [topic, setTopic] = useState<TopicItem>();
+  const [photoModalOn, setPhotoModalOn] = useState(true);
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
   useEffect(() => {
     async function getTopic() {
       const topic = await topicDetailQuery(id);
@@ -19,6 +21,11 @@ export default function Page({ params: { id } }: Props) {
     }
     getTopic();
   }, [id, topicDetailQuery]);
+
+  const handleImageClick = (imageUrl: string) => {
+    setPhotoModalOn(true);
+    setSelectedImage(imageUrl);
+  };
 
   return (
     <section className="mx-6">
@@ -58,6 +65,7 @@ export default function Page({ params: { id } }: Props) {
                       <button
                         className="h-24 w-24 rounded overflow-hidden"
                         key={image.imageUrl}
+                        onClick={() => handleImageClick(image.imageUrl)}
                       >
                         <Image
                           className="rounded object-cover"
@@ -69,6 +77,46 @@ export default function Page({ params: { id } }: Props) {
                       </button>
                     ))}
               </div>
+              {photoModalOn && selectedImage && (
+                <div
+                  className="bg-opacity-50 bg-black z-30 w-[360px] h-[100vh] absolute top-0 -ml-8 flex flex-col justify-center items-center"
+                  onClick={() => setPhotoModalOn(false)}
+                >
+                  <div
+                    className="flex flex-col justify-center items-center"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                    }}
+                  >
+                    <Image
+                      src={selectedImage}
+                      width={280}
+                      height={300}
+                      alt="selected-image"
+                    />
+                    <div className="w-[330px] flex gap-3 overflow-x-scroll scrollbar-hide mt-6 ml-6">
+                      {topic.contentResponses.imageContents.length > 0 &&
+                        topic.contentResponses.imageContents
+                          .filter((image) => image.imageUrl)
+                          .map((image) => (
+                            <button
+                              className="h-24 w-24 rounded overflow-hidden shrink-0"
+                              key={image.imageUrl}
+                              onClick={() => handleImageClick(image.imageUrl)}
+                            >
+                              <Image
+                                className="rounded object-cover"
+                                src={image.imageUrl}
+                                height={96}
+                                width={96}
+                                alt="topic-image"
+                              />
+                            </button>
+                          ))}
+                    </div>
+                  </div>
+                </div>
+              )}
             </div>
           )}
         </>
