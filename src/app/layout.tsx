e99fc189d3a8c825b72d04a696558f5584eb5952
greeneeya <suzythetravelerr@gmail.com>
2024-06-13 16:16:36 +0900

    fix: safari header 사라짐설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,6 +61,11 @@ export default function RootLayout({
       lang="ko"
       className={`${pretendardBoldFont.variable} ${pretendardRegularFont.variable} ${gmarketBoldFont.variable} ${gmarketMediumFont.variable}`}
     >
+      <Head>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <body>
         <ReactQueryClientProvider>
           <OverlayProvider>
